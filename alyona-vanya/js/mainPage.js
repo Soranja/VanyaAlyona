70d7689c3748a261143ab27953b0f4c175fc8f64
Ivan Soranja <98258202+Soranja@users.noreply.github.com>
2023-01-05 19:13:08 +0400

    cat button, cat page started

--- alyona-vanya/js/mainPage.js
+++ alyona-vanya/js/mainPage.js
@@ -1,6 +1,8 @@
 const images = document.querySelectorAll('.column__image');
 const titles = document.querySelectorAll('.column__title');
-// console.log(images, titles);
+const catImg = document.querySelector('#cat');
+const catBtn = document.querySelector('.column__us');
+const meow = document.querySelector('#meow');
 
 images[0].addEventListener('mouseover', function () {
     titles[0].classList.add('title__animation');
@@ -16,4 +18,13 @@ images[1].addEventListener('mouseover', function () {
 
 images[1].addEventListener('mouseout', function () {
     titles[1].classList.remove('title__animation');
+});
+
+catBtn.addEventListener('mouseover', function(){
+    catImg.style.transform = 'rotate(-45deg) translate(60px)';
+    meow.play();
+});
+
+catBtn.addEventListener('mouseout', function(){
+    catImg.style.transform = 'rotate(0deg)';
 });
